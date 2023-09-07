f8a122c8838ead05ebca29293ce1b9a4e828395e
Karácsony Zoltán <karacsony.zolika@gmail.com>
2023-09-07 08:56:49 +0200

    Repository létrehozása

---
+++ asztali_alkalmazasok/alapok20230907/alapok20230907/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alapok20230907
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
