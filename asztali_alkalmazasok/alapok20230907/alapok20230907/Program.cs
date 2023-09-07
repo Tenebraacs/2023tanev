088d4c48b11e6daa715adc203a3dfe0ad93bc54c
Karácsony Zoltán <karacsony.zolika@gmail.com>
2023-09-07 10:39:53 +0200

    Első fejlesztő óra

--- asztali_alkalmazasok/alapok20230907/alapok20230907/Program.cs
+++ asztali_alkalmazasok/alapok20230907/alapok20230907/Program.cs
@@ -10,6 +10,145 @@ namespace alapok20230907
     {
         static void Main(string[] args)
         {
+            // Egysoros megjegyzés
+
+            /*
+             * Több
+             * soros
+             * megjegyzés
+             */
+
+
+            /*
+             * Szintaktikai szabályok:
+             * 
+             *  - Az utasítások végét pontos vesszővel kell lezárni  ->  ';'
+             *  
+             *  - A  kis- és nagybetűs írást meg kell különböztetni
+             *  
+             *  - A program egységeit blokkokba foglaljuk  ->  '{}'
+             *  
+             *  - Minden zárójelnek van pára
+             */
+
+
+            // Kiíró utasítások
+
+            Console.WriteLine("Első (amúgy nem az) C# programom!");
+            Console.Write("Hello World!");
+            Console.Write("Szia világ! :)");
+            Console.WriteLine();
+            // Console.Write(); - üresen nem működik
+            Console.Write("\n"); // Ez ugyan azt eredményezi mint a 'Console.WriteLine();'  ->  sortörés
+
+
+            /*
+             * Változók, és típusai:
+             * 
+             *  - Értékadás (az egyenlőségjel):
+             *      - bal oldalán aminek az értékét szeretnénk megadni
+             *      - jobb oldalán az érték
+             *      
+             *  - Változó: 
+             *      - Névvel ellátott memóriaszelet
+             *      - Módosítható, lekérdezhető
+             *      
+             *  - Konstans:
+             *      - Értéke állandó, lekérdezhető
+             *      - Definiálása: 'const'
+             *      
+             *  - Típus: meghatározzza, hogy a változó milyen halmazból vehet fel és milyen műveletek végezhetőek vele
+             *  
+             *  - Deklaráció: A váltizó nevének és tíusának meghatározása
+             *  
+             *  - Kezdőérték: A születéskor hozzárendelt érték
+             *  
+             *  - Vátozó létrehozásának szintaktikája:
+             *  
+             *      típus változónév = kezdőértek;
+             *      
+             *      
+             *  Típusok csoportosítása:
+             *  
+             *   - Egyszerű típusok:
+             *      - egész
+             *      - valós
+             *      - logikai
+             *      - karakter
+             *      - felsorolás
+             *      
+             *   - Összetett tíusok:
+             *      - szöveg
+             *      - vektor/tömb
+             *      - rekord (struktúra)
+             *      - lista
+             *      - verem
+             *      - sor
+             *      - halmaz
+             *      
+             *      
+             *  Egész számok:
+             *  
+             *   - byte  ->  0-255
+             *   - sbyte  ->  -128 - 127
+             *   - short  ->  -32768 - 32767
+             *   - ushort  -> 0 - 65535
+             *   - int
+             *   - uint
+             *   - long
+             *   - ulong
+             *   
+             *   - Egész számok műveletei:
+             *      - összeadás (+)
+             *      - kivonás (-)
+             *      - szorzás (*)
+             *      - osztás
+             *          - mod (maradék) (%)
+             *          - div (egészrész) (/)
+             *          15 div 2 = 7
+             *          15 mod 2 = 1
+             */
+
+
+            // 1. feladat
+
+            int a = 5;
+            int b = 3;
+            Console.WriteLine("Az első szám: {0}, A második szám: {1}", a, b);
+
+
+            // 2. feladat
+
+            int szam1 = 4, szam2 = 2;
+            Console.WriteLine("szam1 * szam2 = {0}", szam1 * szam2);
+
+
+            /*
+             * Valós számok:
+             * 
+             *  - float
+             *  - double
+             *  - decimal
+             *  - Műveleteik: +, -, *, / 
+             */
+
+
+            // 3. feladat
+
+            double szam3 = 5.8, szam4 = 2.59;
+            Console.WriteLine("szam3 / szam4 = {0:0.00}", szam3 / szam4);
+
+
+            /*
+             * Karakter:
+             *  
+             *  - deklerálása: 'char'
+             */
+
+            char k = 'a';
+            char ures = ' ';
+
+            Console.ReadKey();
         }
     }
 }
