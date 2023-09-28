d30e9cd0af0ccebb5407e9a48c4552c26245ce02
Tenebraacs <119301573+Tenebraacs@users.noreply.github.com>
2023-09-28 09:26:55 +0200

    Update Program.cs

--- asztali_alkalmazasok/2023_09_28/2023_09_28/Program.cs
+++ asztali_alkalmazasok/2023_09_28/2023_09_28/Program.cs
@@ -32,6 +32,27 @@ namespace _2023_09_28
                 negyed = "A pont a tengelyeken vagy a tengelyek között van";
 
             Console.WriteLine($"A pont a(z) {negyed}-ben van.");
-        }
-    }
+
+
+            Console.Write("Kérem, adja meg a tanuló elért pontszámát: ");
+        int pontszam = Convert.ToInt32(Console.ReadLine());
+
+        double szazalek = (pontszam / 150.0) * 100;
+
+        string erdemjegy;
+
+        if (szazalek >= 0 && szazalek <= 19)
+            erdemjegy = "elégtelen";
+        else if (szazalek >= 20 && szazalek <= 39)
+            erdemjegy = "elégséges";
+        else if (szazalek >= 40 && szazalek <= 59)
+            erdemjegy = "közepes";
+        else if (szazalek >= 60 && szazalek <= 79)
+            erdemjegy = "jó";
+        else if (szazalek >= 80 && szazalek <= 100)
+            erdemjegy = "jeles";
+        else
+            erdemjegy = "Érvénytelen pontszám";
+
+        Console.WriteLine($"Az érdemjegy: {erdemjegy}");
 }
