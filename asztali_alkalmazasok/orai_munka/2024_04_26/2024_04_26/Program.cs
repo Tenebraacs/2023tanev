a666adf4466b71ea872b14bc7a08c03ee7a9102d
Karácsony Zoltán <karacsony.zolika@gmail.com>
2024-04-26 13:06:54 +0200

    asd

---
+++ asztali_alkalmazasok/orai_munka/2024_04_26/2024_04_26/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_04_26
+{
+    class Matematika
+    {
+        public double GombTerfogat(double sugar)
+        {
+            // Számolja ki a gömb térfogatát a megadott sugar alapján.
+            double pi = 3.14159265359;
+            double terfogat = (4.0 / 3.0) * pi * Math.Pow(sugar, 3);
+            return terfogat;
+        }
+
+        public double TeglalapTerulet(double a, double b)
+        {
+            // Számolja ki a téglalap területét a megadott oldalhosszak alapján.
+            double terulet = a * b;
+            return terulet;
+        }
+
+        public double TeglalapKerulete(double a, double b)
+        {
+            // Számolja ki a téglalap kerületét a megadott oldalhosszak alapján.
+            double kerulet = 2 * (a + b);
+            return kerulet;
+        }
+
+        public Tuple<double, double, double> Hatvanyok(double szam)
+        {
+            // Számolja ki a megadott szám négyzetét, köbét és negyedik hatványát.
+            double negyzet = Math.Pow(szam, 2);
+            double kob = Math.Pow(szam, 3);
+            double negyedikHatvany = Math.Pow(szam, 4);
+            return Tuple.Create(negyzet, kob, negyedikHatvany);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Példányosítás
+            Matematika matek = new Matematika();
+
+            // Példa a függvények használatára
+            double sugar = 5;
+            Console.WriteLine($"A gömb térfogata {matek.GombTerfogat(sugar)} egység.");
+
+            double a = 4;
+            double b = 3;
+            Console.WriteLine($"A téglalap területe {a}x{b} oldalhossz esetén {matek.TeglalapTerulet(a, b)} egység.");
+            Console.WriteLine($"A téglalap kerülete {a}x{b} oldalhossz esetén {matek.TeglalapKerulete(a, b)} egység.");
+
+            double szam = 2;
+            var hatvanyok = matek.Hatvanyok(szam);
+            Console.WriteLine($"{szam} négyzete: {hatvanyok.Item1}, köbe: {hatvanyok.Item2}, negyedik hatványa: {hatvanyok.Item3}");
+        }
+    }
+}
---
+++ asztali_alkalmazasok/orai_munka/2024_04_26_1/2024_04_26_1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_04_26_1
+{
+    internal class Program
+    {
+        static void parose(int x)
+        {
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("Ez a szám páros");
+            }
+            else
+            {
+                Console.WriteLine("Ez a szám páratlan");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Készítsünk függvényt, amely eldönti, hogy a paramétere páros vagy páratlan természetes szám!
+
+            Console.Write("Írjon be egy tetszőleges természetes számot: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            parose(x);
+
+
+            // Írjunk olyan programot, amely bekér egy mondatot, és kiírja úgy, hogy minden szavának kezdőbetűjét nagybetűvé alakítja!
+
+            Console.Write("Írjon be egy mondatot: ");
+            string mondat = Console.ReadLine();
+            StringBuilder sb = new StringBuilder(mondat);
+
+            bool szo = true;
+            for (int i = 0; i < mondat.Length; i++)
+            {
+                if (szo)
+                {
+                    sb[i] = Char.ToUpper(mondat[i]);
+                    szo = false;
+                }
+                if (mondat[i] == ' ')
+                {
+                    szo = true;
+                }
+            }
+            mondat = sb.ToString();
+            Console.WriteLine(mondat);
+
+
+            // Írjunk olyan programot, amely bekér egy mondatot, és kiírja minden második karakterét! 
+            Console.Write("Írjon be egy mondatot: ");
+            string mondat2 = Console.ReadLine();
+
+            for(int i = 1; i < mondat2.Length; i++)
+            {
+                if(i % 2 != 0)
+                {
+                    Console.Write(mondat2[i]);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
